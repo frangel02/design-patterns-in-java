1f722269e3c173e157cb31831b84d49413083a64
frodriguez <frangele331@gmail.com>
2019-02-14 00:47:02 -0400

    Singleton

--- Singleton/src/SingleObject.java
+++ Singleton/src/SingleObject.java
@@ -7,4 +7,23 @@
  * Created by frodriguez on
  */
 public class SingleObject {
+
+    //create an object of SingleObject
+    private static SingleObject instance = new SingleObject();
+
+    //Make the Constructor private so that this class cannot be
+    //instantiated
+
+    private SingleObject(){
+
+    }
+
+    //Get the only Object available
+    public static SingleObject getInstance(){
+        return instance;
+    }
+
+    public void showMessage(){
+        System.out.println("Hello World!");
+    }
 }
--- Singleton/src/SingletonPatternDemo.java
+++ Singleton/src/SingletonPatternDemo.java
@@ -1,6 +1,14 @@
-public class Main {
+public class SingletonPatternDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //illegal Construct
+        //Compile time error: The constructr SingleObject() is not visible
+        //SingleObject object = new SingleObject();
+
+        //Get only object available
+        SingleObject object = SingleObject.getInstance();
+
+        //Show message
+        object.showMessage();
     }
 }
