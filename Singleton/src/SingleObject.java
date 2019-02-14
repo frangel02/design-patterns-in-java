f9ad8324db7c0fd6174aac1d02cb3308ed16921c
frodriguez <frangele331@gmail.com>
2019-02-14 00:46:18 -0400

    Singleton

---
+++ Singleton/src/SingleObject.java
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: frodriguez
+ * SingleObject ${FILENAME}
+ * Date: 2/14/2019
+ * Time: 12:37 AM
+ * Created by frodriguez on
+ */
+public class SingleObject {
+}
